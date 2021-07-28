2c167a240dd4c9007dbb4e2c0d22dae43eae912a
clairefg <80406038+clairefg@users.noreply.github.com>
2021-07-28 15:41:38 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -5,23 +5,23 @@ var config = {
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: false,
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'Slavery & Resistance in NYC (1626-1865)',
+    subtitle: 'A Walking Tour',
+    byline: 'Created by Mariame Kaba',
     footer: 'Source: source citations, etc.',
     chapters: [
         {
-            id: 'slug-style-id',
+            id: 'Fort Amsterdam',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'Fort Amsterdam',
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                center: [-74.01447, 40.70466],
+                zoom: 17.94,
+                pitch: 60.00,
+                bearing: 26.33
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
