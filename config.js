43153551f45b0a2b5bc6d1e69f3aee0f25ef4b87
clairefg <80406038+clairefg@users.noreply.github.com>
2021-08-02 09:58:31 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
         {
             id: 'Tour Overview',
             alignment: 'left',
-            hidden: true,
+            hidden: false,
             title: 'Tour Overview',
             image: './path/to/image/source.png',
             description: '',
@@ -45,7 +45,7 @@ var config = {
             hidden: false,
             title: 'Fort Amsterdam',
             image: './path/to/image/source.png',
-            description: 'Fort Amsterdam was originally built on the southern tip of Manhattan, at the juncture of the Hudson and East rivers. This is the place where Peter Miniut supposedly purchased Manhattan for $24. The Fort served as the main administrative center for Dutch rule of New Netherlands, and then for English rule. It was torn down following the American Revolution in 1790. <br> <br> The Alexander Hamilton U.S. Custom House was erected on the site in the early 1900s, as a center for the collection of duties from the Port of New York. The building covers three city blocks and contains 450,000 square feet. It was condemned in the 1970s and even scheduled for demolition, but was restored in the 1980s. The building currently houses the National Archives and a bankruptcy court. The National Museum of the American Indian was opened in the Custom House in 1994. <br> <br> The Customs House was lavishly decorated when it was constructed in the early 20th century. Architect Gilbert Cass designed the building and worked with sculptors to create and install appropriate statuary. Since the structure was dedicated to trade, Cass wanted the decorations to also focus on the themes of internationalism and global power. <br> <br> In short, Cass wanted to create a giant tribute to US money and US imperialism at a time of aggressive United States expansion. The United States had fought a war to seize Cuba and the Philippines from Spain in 1898. It had fought another brutal war to put down a rebellion in the Philippines immediately thereafter which lasted until 1913. During this period the US also invaded Panama, Honduras, Nicaragua, Mexico, Haiti, and the Dominican Republic to force these countries to give preferential treatment to US trade and US companies. The imagery used on the building was not a celebration of equality and diversity, but of America’s ability to impose its will on a global scale.',
+            description: 'Fort Amsterdam was originally built on the southern tip of Manhattan, at the juncture of the Hudson and East rivers. This is the place where Peter Miniut supposedly purchased Manhattan for $24. The Fort served as the main administrative center for Dutch rule of New Netherlands, and then for English rule. It was torn down following the American Revolution in 1790. <br> <br> The Alexander Hamilton U.S. Custom House was erected on the site in the early 1900s, as a center for the collection of duties from the Port of New York. The building covers three city blocks and contains 450,000 square feet. It was condemned in the 1970s and even scheduled for demolition, but was restored in the 1980s. The building currently houses the National Archives and a bankruptcy court. The National Museum of the American Indian was opened in the Custom House in 1994. <br> <br> The Customs House was lavishly decorated when it was constructed in the early 20th century. Architect Gilbert Cass designed the building and worked with sculptors to create and install appropriate statuary. Since the structure was dedicated to trade, Cass wanted the decorations to also focus on the themes of internationalism and global power. <br> <br> In short, Cass wanted to create a giant tribute to US money and US imperialism at a time of aggressive United States expansion. The United States had fought a war to seize Cuba and the Philippines from Spain in 1898. It had fought another brutal war to put down a rebellion in the Philippines immediately thereafter which lasted until 1913. During this period the US also invaded Panama, Honduras, Nicaragua, Mexico, Haiti, and the Dominican Republic to force these countries to give preferential treatment to US trade and US companies. The imagery used on the building was not a celebration of equality and diversity, but of America’s ability to impose its will on a global scale. <br> <br> The top of the building features twelve figures representing great naval and commercial nations of the past. Sculpted heads symbolize the races of the world. There are also images of Mercury, who was the Roman god of trade.',
             
             location: {
                 center: [-74.01366, 40.70452],
@@ -61,12 +61,11 @@ var config = {
         },
         {
             id: '2',
-            alignment: 'right',
+            alignment: 'full',
             hidden: false,
             title: 'Fort Amsterdam',
             image: './path/to/image/source.png',
-            description: '',
-            
+            description: 'Perhaps the most striking feature is <i>The Four Continents</i>, a series of four enormous sculptures on the building’s entrance pedestals. These were designed by Daniel Chester French, who would go on to design the Lincoln Memorial in 1920. <br> <br> Many drawings and prints and other small-scale works had been dedicated to "the four continents" since the existence of the Americas became widely known in Europe at the end of the 15th Century. It was unusual to treat the theme in large scale sculpture though. <br> <br> French’s statues on the theme each show a female figure surrounded by other individuals and objects meant to represent the four continents: Asia, America, Europe, and Africa.',
             location: {
                 center: [-74.01366, 40.70452],
                 zoom: 19.05,
@@ -85,8 +84,8 @@ var config = {
             hidden: false,
             title: 'Fort Amsterdam',
             image: './path/to/image/source.png',
-            description: '',
-            
+            description: 'When viewed from left to right, the first sculpture is <b>Asia</b>. It is presented as the place where the world’s major religions began. That is, from Cass’s American imperialist perspective, Asia is important for what it did in the <i>past</i>, rather than for its present. The implication is that Europe and the Americas have rightfully succeeded Asia as the leaders of the world. <br> <br> The central figure of this group is a calm woman, seated with eyes closed, her hands resting on her knees. To the woman’s right is a tiger, gazing up at the central figure. This tableau symbolizes religion taming the wildest beast. <br> <br> On the woman’s lap there sits a small Buddha, which Cass said was an “idol, symbol of false worship”. Over the right shoulder rises what Cass called “the rising luminous cross of Christianity, symbol of hope, which found its birth place on the continent of Asia.” Note that the early twentieth century was a period of intense missionary activity, when many white Americans were eager to travel abroad to convert non-Christians. In linking Christianity with American trade, Cass is draping commercial interests in spiritual moralism. <br> <br> The woman is seated on a throne supported by skulls, which represents those killed through forced labor. To the woman’s left there are a group of huddled figures, one of whom is bound. The figures according to Cass represent “the hordes of India, and the hopelessness of the life of so many of the inhabitants." <br> <br> Of course, the United States was itself built on the forced labor of slavery itself, and was home to many poor people. Descendants of enslaved Black people were still being brutalized and exploited throughout the Jim Crow south,  and conditions among immigrant communities in New York itself were incredibly crowded and difficult. However, emphasizing the poverty and injustice of foreign nations, and dehumanizing them as “hordes” created justifications for American missionary, military, and economic intervention abroad.',
+
             location: {
                 center: [-74.01366, 40.70452],
                 zoom: 19.05,
